a4a711e8ebc03e312b99cde2fb3fa37177a9692d
Karl Baum <karl.baum@gmail.com>
2014-02-09 08:29:07 -0500

    missing backup name for s3 copy.. whoops\!

--- bin/backup.sh
+++ bin/backup.sh
@@ -7,6 +7,6 @@ BACKUP_FILE_NAME="$APP.$DATABASE.$(date +"%Y.%m.%d.%S.%N").dump"
 /app/vendor/heroku-toolbelt/bin/heroku pgbackups:capture $DATABASE -e --app $APP
 curl -o $BACKUP_FILE_NAME `./vendor/heroku-toolbelt/bin/heroku pgbackups:url --app $APP` 
 gzip $BACKUP_FILE_NAME
-/app/vendor/awscli/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://vts-db-backups/pgbackups
+/app/vendor/awscli/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://vts-db-backups/pgbackups/$BACKUP_FILE_NAME.gz
 echo "backup $BACKUP_FILE_NAME complete"
 
