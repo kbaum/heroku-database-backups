8472beccbed7b1ba2329fd191b6a37414963e84a
Karl Baum <karl.baum@gmail.com>
2014-02-08 22:46:45 -0500

    made backup script a bit more robust

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-BACKUP_FILE_NAME="backup.$(date +"%Y.%m.%d.%S.%N").dump"
-curl -o $BACKUP_FILE_NAME `./vendor/heroku-toolbelt/bin/heroku pgbackups:url --app vts-eu` 
+APP="vts-eu"
+DATABASE="HEROKU_POSTGRESQL_MAROON_URL"
+BACKUP_FILE_NAME="$APP.$DATABASE.$(date +"%Y.%m.%d.%S.%N").dump"
+
+/app/vendor/heroku-toolbelt/bin/heroku pgbackups:capture $DATABASE -e --app $APP
+curl -o $BACKUP_FILE_NAME `./vendor/heroku-toolbelt/bin/heroku pgbackups:url --app $APP` 
 gzip $BACKUP_FILE_NAME
-./vendor/awscli/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://vts-db-backups/pgbackups
+/app/vendor/awscli/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://vts-db-backups/pgbackups
 echo "backup $BACKUP_FILE_NAME complete"
 
