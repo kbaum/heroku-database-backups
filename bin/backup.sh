90a40222bc2a7c1d85065f69dad333ccdefa76bf
Karl Baum <karl.baum@gmail.com>
2014-02-10 11:28:45 -0500

    made script a bit more generic

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-APP="vts-eu"
-DATABASE="HEROKU_POSTGRESQL_MAROON_URL"
-BACKUP_FILE_NAME="$APP.$DATABASE.$(date +"%Y.%m.%d.%S.%N").dump"
+if [[ -z "$APP" ]]; then
+  echo "Missing APP variable which must be set to the name of your app where the db is located" 
+  exit 1
+fi
 
+if [[ -z "$DATABASE" ]]; then
+  echo "Missing DATABASE variable which must be set to the name of the DATABASE you would like to backup"
+  exit 1
+fi
+
+BACKUP_FILE_NAME="$APP.$DATABASE.$(date +"%Y.%m.%d.%S.%N").dump"
 /app/vendor/heroku-toolbelt/bin/heroku pgbackups:capture $DATABASE -e --app $APP
 curl -o $BACKUP_FILE_NAME `./vendor/heroku-toolbelt/bin/heroku pgbackups:url --app $APP` 
 gzip $BACKUP_FILE_NAME
