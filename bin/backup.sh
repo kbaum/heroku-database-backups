f310ec5f983621228372f1e438ebbfefa82f9b01
Oded Lazar <odedlaz@gmail.com>
2015-11-03 14:41:43 +0200

    some minor changes - changed backup name to ISO8601 date - added blob expiration in days (EXPIRE_IN variable) - added more logs - moved all downloas to /tmp

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,6 +3,11 @@
 # terminate script as soon as any command fails
 set -e
 
+if [[ -z "$EXPIRE_IN" ]]; then
+  echo "Missing EXPIRE_IN variable which must be set to the amount of days the backup will expire"
+  exit 1
+fi
+
 if [[ -z "$APP" ]]; then
   echo "Missing APP variable which must be set to the name of your app where the db is located"
   exit 1
@@ -19,16 +24,27 @@ if [[ -z "$S3_BUCKET_PATH" ]]; then
 fi
 
 #install aws-cli
-curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
-unzip awscli-bundle.zip
-chmod +x ./awscli-bundle/install
-./awscli-bundle/install -i /tmp/aws
+echo "downloading aws cli"
+curl --progress-bar -o /tmp/awscli-bundle.zip https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+unzip -qq -d /tmp /tmp/awscli-bundle.zip
+chmod +x /tmp/awscli-bundle/install
+/tmp/awscli-bundle/install -i /tmp/aws
 
-BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.dump"
-
+echo "creating a new backup"
 /app/vendor/heroku-toolbelt/bin/heroku pg:backups capture $DATABASE --app $APP
-curl -o $BACKUP_FILE_NAME `/app/vendor/heroku-toolbelt/bin/heroku pg:backups public-url --app $APP`
-gzip $BACKUP_FILE_NAME
-/tmp/aws/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://$S3_BUCKET_PATH/$APP/$DATABASE/$BACKUP_FILE_NAME.gz
+BACKUP_URL=`/app/vendor/heroku-toolbelt/bin/heroku pg:backups public-url --app $APP | cat`
+BACKUP_FILE_NAME=`echo $BACKUP_URL | awk -F "/" '{print $5}' | sed -e 's/%3A/:/g'`
+
+echo "downloading backup"
+curl --progress-bar -o /tmp/$BACKUP_FILE_NAME $BACKUP_URL
+gzip /tmp/$BACKUP_FILE_NAME
+
+EXPIRATION=$(date -d "$BACKUP_FILE_NAME $EXPIRE_IN days" +"%Y-%m-%dT%H:%M:%SZ")
+echo "uploading backup, it'll expire at: $EXPIRATION"
+
+/tmp/aws/bin/aws s3 cp /tmp/$BACKUP_FILE_NAME.gz s3://$S3_BUCKET_PATH/$APP/$DATABASE/$BACKUP_FILE_NAME.gz --expires $EXPIRATION
 echo "backup $BACKUP_FILE_NAME complete"
 
+# cleaning up
+rm -rf /tmp/aws*
+rm -rf /tmp/$BACKUP_FILE_NAME*
