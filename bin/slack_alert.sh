b7dffd759b29fa5d7cfb300b6edef9b711ce3344
Marina Morris <marina.morris@givegab.com>
2021-11-02 14:00:07 -0400

    add a slack alert if db backups fail

---
+++ bin/slack_alert.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Automatic database backup failed."}' https://hooks.slack.com/services/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
